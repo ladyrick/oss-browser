dddfa7ec7244a821f425cd02931ed62d36011486
ladyrick <ladyrick@qq.com>
2025-08-03 01:08:44 +0800

    auto build and make release
    Signed-off-by: ladyrick <ladyrick@qq.com>

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
+commit_id="$1"
+
 if [[ -d node_modules ]]; then
     yarn install --frozen-lockfile
     yarn build
@@ -9,5 +11,6 @@ fi
 rm -rf oss-browser
 mkdir oss-browser
 cp -r dist run.sh server.py oss-browser
+[[ -n "$commit_id" ]] && echo "$commit_id" >oss-browser/commit_id
 tar zcvf oss-browser.tar.gz oss-browser
 rm -rf oss-browser
